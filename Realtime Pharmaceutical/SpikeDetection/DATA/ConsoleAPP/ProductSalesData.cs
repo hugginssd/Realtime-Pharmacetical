dddfaebfab20c0af708b028a7b0d06ad4ddb4bff
hugginssd <hugginsmafigu@gmail.com>
2019-05-08 14:47:34 +0200

    Spike detection

---
+++ Realtime Pharmaceutical/SpikeDetection/DATA/ConsoleAPP/ProductSalesData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Realtime_Pharmaceutical.SpikeDetection.DATA.ConsoleAPP
+{
+    public class ProductSalesData
+    {
+        //[LoadColumn(0)]
+        public string Month;
+
+        //[LoadColumn(1)]
+        public float numSales;
+    }
+}
---
+++ Realtime Pharmaceutical/SpikeDetection/DATA/ConsoleAPP/ProductSalesPrediction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Realtime_Pharmaceutical.SpikeDetection.DATA.ConsoleAPP
+{
+    public class ProductSalesPrediction
+    {
+        //vector to hold alert,score,p-value values
+        //[VectorType(3)]
+        public double[] Prediction { get; set; }
+    }
+}
