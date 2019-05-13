9f429040fffede9f39366da5d86134280b0eaf70
hugginssd <hugginsmafigu@gmail.com>
2019-05-13 10:40:13 +0200

    Spike detection in Realtime API

---
+++ Realtime/Models/Spike Detection/DATA/ConsoleAPP/ProductsSalesData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Realtime_API.Models.Spike_Detection.DATA.ConsoleAPP
+{   
+    public class ProductsSalesData
+    {
+        //[LoadColumn(0)]
+        public string Month;
+
+        //[LoadColumn(1)]
+        public float numSales;
+    }
+}
---
+++ Realtime/Models/Spike Detection/DATA/ConsoleAPP/ProductsSalesPrediction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Realtime_API.Models.Spike_Detection.DATA.ConsoleAPP
+{   
+    public class ProductsSalesPrediction
+    {
+    //vector to hold alert,score,p-value values
+    //[VectorType(3)]
+    public double[] Prediction { get; set; }
+    }
+}
